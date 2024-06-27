06b054e0e3aea0e54947faca058cce3b9f372a25
Dell <prasath@crayond.co>
2024-06-27 19:43:38 +0530

    webhooks check

---
+++ create-webook-readme.md
@@ -0,0 +1,7 @@
+install:
+    sudo apt-get update
+    sudo apt-get install webhook
+
+web hook configuration:
+    sudo mkdir webhook
+    cd /webhook
---
+++ webhook-script.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install webhook
+
+mkdir -p etc/webhook
+
+cd /etc/webhook
+
+mkdir -p web_hooks.json
+
+# enter web hook id
+echo "Enter ID for webhook: "
+read webhook_id
+echo "Your Webhook id , $name!"
+
+
+# generate secret
+# openssl rand -hex 32
+
+[
+  {
+    "id": "deploy-node-app",
+    "execute-command": "/home/dell/deploy.sh",
+    "command-working-directory": "/home/dell/deployment",
+    "trigger-rule": {
+      "and": [
+        {
+          "match": {
+            "type": "payload-hash-sha1",
+            "secret": "271fdf24c79ba0a4d87649663de147bb72d6c91e7b189f496495cefeef2dc6bc",
+            "parameter": {
+              "source": "header",
+              "name": "X-Hub-Signature"
+            }
+          }
+        },
+        {
+          "match": {
+            "type": "value",
+            "value": "push",
+            "parameter": {
+              "source": "payload",
+              "name": "action"
+            }
+          }
+        }
+      ]
+    }
+  }
+]
+
+webhook -hooks /etc/webhook/hooks.json -port 9000
